ba5dd3a4bc5862e52cb2fa87db3bc25cf989c7d9
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-07 14:33:20 +0000

    Remove Deprecated UseNewUrlParser From MongoConfig

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+
 
 const indexRouter = require('./routes/index');
 
--- mongoConfig.js
+++ mongoConfig.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const mongoDB = process.env.MONGODB_URI;
 
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
