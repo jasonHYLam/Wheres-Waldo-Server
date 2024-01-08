519d05d66c2558acdf6af2f19c6f03fcb08093cb
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-08 14:35:38 +0000

    Rename Get_Char Callback To Get_Chars

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,9 @@ router.get('/',(req, res, next) => {
 
 });
 
-router.get('/get_char', controller.get_char)
+router.get('/get_char', controller.get_chars)
 
+router.get('/get_char/:name', controller.get_specific_char)
 // create get for getting database character info
 
 // create get for all character info
