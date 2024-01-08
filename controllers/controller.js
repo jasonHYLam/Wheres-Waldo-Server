50514805be23eab545b71ea4c243bfa713fa0f46
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-08 16:58:15 +0000

    Change Route+Verb For Get_Specific_Char Callback

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,9 +4,17 @@ const Character = require('../models/character');
 
 exports.get_chars = asyncHandler( async(req, res, next) => {
     const allCharacters = await Character.find({}).exec()
+    console.log('doki doki waku waku')
     res.json(allCharacters)
 })
 
 exports.get_specific_char = asyncHandler( async(req, res, next) => {
     console.log('game sucks')
+    console.log(req.params)
+    console.log('nanka saa')
+    console.log(req.body)
+
+    const character = await Character.find({name: req.params.name}).exec()
+    console.log('aquarius')
+    console.log(character)
 })
