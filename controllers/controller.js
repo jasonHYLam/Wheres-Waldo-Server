f0b7607aaf233ebe55be8e8d4032590523fcf269
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-08 14:36:35 +0000

    Create Get_specific_char Callback

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,11 @@ const asyncHandler = require('express-async-handler');
 
 const Character = require('../models/character');
 
-exports.get_char = asyncHandler( async(req, res, next) => {
+exports.get_chars = asyncHandler( async(req, res, next) => {
     const allCharacters = await Character.find({}).exec()
     res.json(allCharacters)
+})
+
+exports.get_specific_char = asyncHandler( async(req, res, next) => {
+    console.log('game sucks')
 })
