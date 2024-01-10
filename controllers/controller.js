0c228b0c71ed201c08d0a58900d844cf32da1556
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-10 15:18:53 +0000

    Update Coords Reference In Req.Body
    The coords reference changed from coords to normalisedCoords.

--- controllers/controller.js
+++ controllers/controller.js
@@ -10,13 +10,15 @@ exports.get_chars = asyncHandler( async(req, res, next) => {
 exports.get_specific_char = asyncHandler( async(req, res, next) => {
 
     const [ character ] = await Character.find({name: req.body.name}).exec()
+    console.log('character')
+    console.log(character)
     // compare x and y oordinates seperately, in the given range.
     // perhaps better if i swap such that range of coords is used.
     // i chose to use range of mouseCoords because of the targetBox.
     if (
-        (character.coords.x < (req.body.mouseCoords.x + 30) && character.coords.x > (req.body.mouseCoords.x - 30))
+        (character.coords.x < (req.body.normalisedCoords.x + 30) && character.coords.x > (req.body.normalisedCoords.x - 30))
         &&
-        (character.coords.y < (req.body.mouseCoords.y + 30) && character.coords.y > (req.body.mouseCoords.y - 30))
+        (character.coords.y < (req.body.normalisedCoords.y + 30) && character.coords.y > (req.body.normalisedCoords.y - 30))
         )
         {
             res.json({isCorrect: true})
