0476cea473369b4c677c937786a34b44740c00a7
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-15 13:39:29 +0000

    Create New UserAndScore Document And Save

--- controllers/scoreController.js
+++ controllers/scoreController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const { body } = require('express-validator');
+const he = require('he');
 
 const UserAndScore = require('../models/userAndScore');
 
@@ -10,8 +11,13 @@ exports.submit_score = [
     .escape(),
 
     asyncHandler( async(req, res, next) => {
-        // might need to unescape this
-        console.log(req.body)
+
+        const userAndScore = new UserAndScore({
+            name: he.decode(req.body.name),
+            timeInMs: req.body.timerValue,
+        })
+
+        await userAndScore.save();
     })
 
 ]
