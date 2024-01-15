24d449def9c09056e2e633c68deb85595507c9c1
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-15 13:54:07 +0000

    Create Get_Leaderboard Route

--- routes/index.js
+++ routes/index.js
@@ -14,20 +14,21 @@ router.get('/',(req, res, next) => {
 
 });
 
-router.get('/get_char', characterController.get_chars)
+router.get('/get_char', characterController.get_chars);
 
-router.post('/get_specific_char', characterController.get_specific_char)
+router.post('/get_specific_char', characterController.get_specific_char);
 // create get for getting database character info
 
 // create get for all character info
 
 // get for starting timer
-router.get('/start_game', timerController.start_timer)
+router.get('/start_game', timerController.start_timer);
 
 // get for stopping timer
-router.get('/stop_game', timerController.stop_timer_and_get_value)
+router.get('/stop_game', timerController.stop_timer_and_get_value);
 
-router.post('/submit_score', scoreController.submit_score)
+router.post('/submit_score', scoreController.submit_score);
+router.get('/get_leaderboard', scoreController.get_all_scores);
 
 
 module.exports = router;
